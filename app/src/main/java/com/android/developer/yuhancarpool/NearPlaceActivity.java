91130fd51bd56eb32ff263038ed7532450b1eb1d
mobileapp201827012songyuna <ski05028@naver.com>
2020-06-17 06:30:01 +0900

    0617 commit

--- app/src/main/java/com/android/developer/yuhancarpool/NearPlaceActivity.java
+++ app/src/main/java/com/android/developer/yuhancarpool/NearPlaceActivity.java
@@ -1,97 +1,124 @@
 package com.android.developer.yuhancarpool;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-public class NearPlaceActivity extends AppCompatActivity {
-    private Button button18;
-    private TextView textView5;
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+public class NearPlaceActivity extends AppCompatActivity
+        implements AutoPermissionsListener {
+
+  private static final String TAG = "LOCATION";
+  TextView textView5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_near_place);
-        button18 = (Button)findViewById(R.id.button18);
-        textView5 = (TextView)findViewById(R.id.textView5);
 
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        textView5 = findViewById(R.id.textView5);
+
+        Button button18 = findViewById(R.id.button18);
         button18.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( Build.VERSION.SDK_INT >= 23 &&
-                        ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-                    ActivityCompat.requestPermissions( NearPlaceActivity.this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                            0 );
-                }
-                else{
-                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    String provider = location.getProvider();
-                    double longitude = location.getLongitude();
-                    double latitude = location.getLatitude();
-                    double altitude = location.getAltitude();
-
-                    textView5.setText("위치정보 : " + provider + "\n" +
-                            "위도 : " + longitude + "\n" +
-                            "경도 : " + latitude + "\n" +
-                            "고도  : " + altitude);
-
-                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                            1000,
-                            1,
-                            gpsLocationListener);
-                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                            1000,
-                            1,
-                            gpsLocationListener);
-                }
+                //버튼 클릭 시 GPS에서 사용자의 위치를 가져오는 코드
+                startLocationService();
             }
         });
 
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+
     }
-    final LocationListener gpsLocationListener = new LocationListener() {
+
+    public void startLocationService() {
+        LocationManager manager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            Location location = manager
+                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if(location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                Log.d(TAG,"startLocationService()");
+                String message = "최근 위치 -> 위도 : " +latitude
+                        + "\n 경도 : " + longitude;
+
+                textView5.setText(message);
+
+                GPSListener gpsListener = new GPSListener();
+                long minTime = 10000;
+                float minDistance = 0;
+
+                manager.requestLocationUpdates
+                        (LocationManager.GPS_PROVIDER,minTime,
+                                minDistance,gpsListener );
+            }
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDenied(int i, String[] strings) {
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions
+                .Companion
+                .parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onGranted(int i, String[] strings) {
+
+    }
+
+    class GPSListener implements LocationListener {
+
+        @Override
         public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
 
-            String provider = location.getProvider();
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-            double altitude = location.getAltitude();
-
-            textView5.setText("위치정보 : " + provider + "\n" +
-                    "위도 : " + longitude + "\n" +
-                    "경도 : " + latitude + "\n" +
-                    "고도  : " + altitude);
-
+            String message = "최근 위치 -> 위도 : " +latitude
+                    + "\n 경도 : " + longitude;
+            textView5.setText(message);
+            Log.d(TAG,"onLocationChanged()");
         }
 
+        @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
+
         }
 
+        @Override
         public void onProviderEnabled(String provider) {
+
         }
 
+        @Override
         public void onProviderDisabled(String provider) {
+
         }
-    };
-
-
-
-
-
-
-
-
+    }
 
     }
 
